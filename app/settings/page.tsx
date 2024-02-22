a48eda1afeba6e6e91e3593a733d98524ee0b84e
nv1327 <n.vitebsky1327@gmail.com>
2024-02-22 16:44:32 -0600

    updated

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -85,7 +85,7 @@ export default async function SettingsPage() {
                             <div className="col-span-12">
                                 <div>
                                     <label className="block text-sm font-medium text-gray-700">Grade Level</label>
-                                    <div className="relative mt-2 rounded-md shadow-sm border border-black pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    <div className="relative mt-2 rounded-md shadow-sm border border-black pl-2 block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                         <select name="grade_level" id="grade_level" className="w-full" value={data?.[0]?.grade_level || 'Select Grade Level'}>
                                             <option selected disabled>Select Grade Level</option>
                                             <option value="Freshman">Freshman</option>
