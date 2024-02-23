36e041d60c00420ab0f77b429757261b5fb7012d
nv1327 <n.vitebsky1327@gmail.com>
2024-02-23 11:48:50 -0600

    id page

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -21,15 +21,41 @@ export default async function EventsIndividualPage({ params }: { params: { id: s
     const user = session?.user;
     const event_id = params.id;
 
+    //check for vanderbilt email using this
+    function getSecondPart(str: string) {
+        return str.split('@')[1];
+    }
+
+    if (user && user.email) {
+        if (getSecondPart(user.email) != "vanderbilt.edu") {
+            console.log("Not a Vandy email");
+            return redirect('/invalidemail');
+        }
+    } else {
+        console.log("User or user.email is undefined");
+    }
+
+    //---- vanderbilt email check function above
+
     const supabase = createServerActionClient<Database>({ cookies });
-    const { data: siteData, error: siteError } = await supabase
+    const { data: event, error: eventError } = await supabase
         .from('events')
         .select('*')
         .eq('id', params.id);
-    if (siteError) {
-        console.error("Error getting event", siteError);
+    if (eventError) {
+        console.error("Error getting event", eventError);
     } else {
-        console.log("Event retrieved:", siteData);
+        console.log("Event retrieved:", event);
+    }
+
+    const { data: ownerData, error: ownerError } = await supabase
+        .from('users')
+        .select('*')
+        .eq('id', event && event[0].user_id);
+    if (ownerError) {
+        console.error("Error getting owner", ownerError);
+    } else {
+        console.log("Owner retrieved:", ownerData);
     }
 
     return (
@@ -61,8 +87,8 @@ export default async function EventsIndividualPage({ params }: { params: { id: s
                                 <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z">
                                 </path>
                             </svg>
-                            <a className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" href={"/events/" + (siteData && siteData[0].id)}>
-                                {siteData && siteData[0].name}
+                            <a className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" href={"/events/" + (event && event[0].id)}>
+                                {event && event[0].name}
                             </a>
                         </div>
                     </li>
@@ -72,22 +98,56 @@ export default async function EventsIndividualPage({ params }: { params: { id: s
                 <div className="sm:hidden">
                     <label htmlFor="tabs" className="sr-only">Select a tab</label>
                     <select id="tabs" name="tabs" className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                        <option value="/events/[id]">Article Reports</option>
+                        <option value="/events/[id]">Overview</option>
                         <option value="/events/[id]/settings">Settings</option>
                     </select>
                 </div>
                 <div className="hidden sm:block">
                     <div className="border-b border-gray-200">
                         <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-                            <a className="border-indigo-500 text-indigo-600 cursor-pointer whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm" href={"/events/" + (siteData && siteData[0].id)}>
-                                Article Reports
+                            <a className="border-indigo-500 text-indigo-600 cursor-pointer whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm" href={"/events/" + (event && event[0].id)}>
+                                Overview
                             </a>
-                            <a className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 cursor-pointer whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm" href={"/events/" + (siteData && siteData[0].id) + "/settings"}>
+                            <a className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 cursor-pointer whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm" href={"/events/" + (event && event[0].id) + "/settings"}>
                                 Settings
                             </a>
                         </nav>
                     </div>
                 </div>
+                <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+                <div className="px-4 py-5 sm:px-6">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">{event && event[0].name}</h3>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500">Hosted by {ownerData && ownerData[0].first_name + " " + ownerData[0].last_name}</p>
+                </div>
+                <div className="border-t border-gray-200">
+                    <dl>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Date</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{event && event[0].date}</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Time</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{event && event[0].start_time} - {event && event[0].end_time}</dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Location</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{event && event[0].location}</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Description</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{event && event[0].description}</dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Equipment Needed</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{event && event[0].equipment}</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Number of Spots</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{event && event[0].num_spots}</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
             </div>
         </div>
     );
